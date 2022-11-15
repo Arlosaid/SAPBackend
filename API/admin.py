f8d1c221dda34556e6427b4d2e8cc0491a0eee69
Alonso <adroal08@gmail.com>
2022-11-15 12:49:36 -0600

    EmployeesSubdivision model was removed

--- API/admin.py
+++ API/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Roles, Divisions, Subdivisions, Employees, EmployeesSubdivisions
+from .models import Roles, Divisions, Subdivisions, Employees
 
 # Register your models here.
 admin.site.register(Roles)
 admin.site.register(Divisions)
 admin.site.register(Subdivisions)
 admin.site.register(Employees)
-admin.site.register(EmployeesSubdivisions)
