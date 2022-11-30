ad6460de4b3d633c76ed50840ec681f2f2cb070f
Ubuntu <ubuntu@ip-172-31-83-76.ec2.internal>
2022-11-30 19:53:30 +0000

    API to api

--- core/settings.py
+++ core/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'DBSAP',
+       'NAME': 'sapemployees',
        'USER': 'postgres',
        'PASSWORD': 'admin',
        'HOST': 'localhost',
